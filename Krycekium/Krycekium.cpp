5b689441b85bdb6b54e2c6435e07636ea7502806
Force Charlie <charlieio@outlook.com>
2018-08-19 20:53:59 +0800

    update code only support Windows 10, code C++17

--- Krycekium/Krycekium.cpp
+++ Krycekium/Krycekium.cpp
@@ -34,10 +34,9 @@ int MetroWindowRunLoop() {
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
                     _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
-  if (!IsWindows7SP1OrGreater()) {
-    MessageBoxW(nullptr, L"You need at least Windows 7",
-                L"Please upgrade Your OS to Windows 7 or Windows 10",
-                MB_OK | MB_ICONERROR);
+  if (!IsWindows10OrGreater()) {
+    MessageBoxW(nullptr, L"You need at least Windows 10",
+                L"Please upgrade Your OS to Windows 10", MB_OK | MB_ICONERROR);
     return -1;
   }
   DotComInitialize dot;
